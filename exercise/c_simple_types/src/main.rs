65226eacfa5a3e567ad53edcba59fbbd43272a48
Satryo <satryo220293@gmail.com>
2025-10-09 10:51:08 +0700

    complete exercise C

--- exercise/c_simple_types/src/main.rs
+++ exercise/c_simple_types/src/main.rs
@@ -1,5 +1,6 @@
 // Silence some warnings so they don't distract from the exercise.
 #![allow(dead_code, unused_variables)]
+use ding_machine::{print_difference, print_array, print_distance, ding, on_off};
 
 fn main() {
     let coords: (f32, f32) = (6.3, 15.0);
@@ -10,15 +11,16 @@ fn main() {
     // at how it is defined.
     //
     //print_difference( ... );   // Uncomment and finish this line
-
+    print_difference(coords.0, coords.1);
 
     // 2. We want to use the `print_array` function to print coords...but coords isn't an array!
     // Create an array of type [f32; 2] and initialize it to contain the
     // information from coords.  Uncomment the print_array line and run the code.
-    //
+    //y.p
     //let coords_arr...               // create an array literal out of parts of `coord` here
     //print_array(coords_arr);        // and pass it in here (this line doesn't need to change)
-
+    let coords_arr: [f32;2] = coords.into();
+    print_array(coords_arr);
 
     let series = [1, 1, 2, 3, 5, 8, 13];
     // 3. Make the `ding` function happy by passing it the value 13 out of the `series` array.
@@ -26,6 +28,7 @@ fn main() {
     // "Ding, you found 13!"
     //
     //ding(...);
+    ding(series[6]);
 
 
     let mess = ([3, 2], 3.14, [(false, -3), (true, -100)], 5, "candy");
@@ -33,6 +36,7 @@ fn main() {
     // `cargo run` will produce the additional output "Lights are on!" I'll get you started:
     //
     //on_off(mess.2 ...);
+    on_off((mess.2[1]).0);
 
     // 5.  What a mess -- functions in a binary! Let's get organized!
     //
@@ -48,36 +52,6 @@ fn main() {
     // output. Then go refactor the print_distance() function according to the
     // instructions in the comments inside that function.
 
-    // print_distance(coords);
-}
+    print_distance(coords);
 
-fn print_difference(x: f32, y: f32) {
-    println!("Difference between {} and {} is {}", x, y, (x - y).abs());
 }
-
-fn print_array(a: [f32; 2]) {
-    println!("The coordinates are ({}, {})", a[0], a[1]);
-}
-
-fn ding(x: i32) {
-    if x == 13 {
-        println!("Ding, you found 13!");
-    }
-}
-
-fn on_off(val: bool) {
-    if val {
-        println!("Lights are on!");
-    }
-}
-
-fn print_distance(z: (f32, f32)) {
-    // Using z.0 and z.1 is not nearly as nice as using x and y.  Lucky for
-    // us, Rust supports destructuring function arguments.  Try replacing "z" in
-    // the parameter list above with "(x, y)" and then adjust the function
-    // body to use x and y.
-    println!(
-        "Distance to the origin is {}",
-        ( z.0.powf(2.0) + z.1.powf(2.0) ).sqrt());
-}
-
